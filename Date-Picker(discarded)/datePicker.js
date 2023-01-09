18c6394b2a52fed6beffe986811f0c4166952d12
hsjaehun <hsjaehun@gmail.com>
2023-01-09 23:24:25 +0900

    playground

--- Date-Picker(discarded)/datePicker.js
+++ Date-Picker(discarded)/datePicker.js
@@ -1,12 +1,31 @@
-const datePickerElement = document.querySelector('.date-picker');
-const selectedDateElement = document.querySelector('.date-picker .selected-date');
-const datesElement = document.querySelector('.date-picker .dates');
-const monthElement = document.querySelector('.date-picker .dates .month .mth');
-const nextMonthElement = document.querySelector('.date-picker .dates .month .next-mth');
-const prevMonthElement = document.querySelector('.date-picker .dates .month .prev-mth');
-const daysElement = document.querySelector('.date-picker .dates .days');
+const datePickerElement = document.querySelector(".date-picker");
+const selectedDateElement = document.querySelector(
+	".date-picker .selected-date"
+);
+const datesElement = document.querySelector(".date-picker .dates");
+const monthElement = document.querySelector(".date-picker .dates .month .mth");
+const nextMonthElement = document.querySelector(
+	".date-picker .dates .month .next-mth"
+);
+const prevMonthElement = document.querySelector(
+	".date-picker .dates .month .prev-mth"
+);
+const daysElement = document.querySelector(".date-picker .dates .days");
 
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const months = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December",
+];
 
 let date = new Date();
 let day = date.getDate();
@@ -18,7 +37,7 @@ let selectedDay = day;
 let selectedMonth = month;
 let selectedYear = year;
 
-monthElement.textContent = months[month] + ' ' + year;
+monthElement.textContent = months[month] + " " + year;
 
 selectedDateElement.textContent = formatDate(date);
 selectedDateElement.dataset.value = selectedDate;
@@ -26,87 +45,91 @@ selectedDateElement.dataset.value = selectedDate;
 populateDates();
 
 // EVENT LISTENERS
-daysElement.addEventListener('wheel', (event) => {
-   if(event.deltaY>0) goToPrevMonth()
-   else if (event.deltaY<0) goToNextMonth()
-})
+daysElement.addEventListener("wheel", (event) => {
+	if (event.deltaY > 0) goToPrevMonth();
+	else if (event.deltaY < 0) goToNextMonth();
+});
 
-function goToNextMonth (e) {
-   month++;
-   if (month > 11) {
-      month = 0;
-      year++;
-   }
-   monthElement.textContent = months[month] + ' ' + year;
-   populateDates();
+function goToNextMonth(e) {
+	month++;
+	if (month > 11) {
+		month = 0;
+		year++;
+	}
+	monthElement.textContent = months[month] + " " + year;
+	populateDates();
 }
 
-function goToPrevMonth (e) {
-   month--;
-   if (month < 0) {
-      month = 11;
-      year--;
-   }
-   monthElement.textContent = months[month] + ' ' + year;
-   populateDates();
+function goToPrevMonth(e) {
+	month--;
+	if (month < 0) {
+		month = 11;
+		year--;
+	}
+	monthElement.textContent = months[month] + " " + year;
+	populateDates();
 }
 
-function populateDates (e) {
-   daysElement.innerHTML = '';
-   let amount_days = 31;
+function populateDates(e) {
+	daysElement.innerHTML = "";
+	let amount_days = 31;
 
-   // month === 1 === Febuary
-   if (month === 1) {
-      amount_days = 28;
-   }
+	// month === 1 === Febuary
+	if (month === 1) {
+		amount_days = 28;
+	}
 
-   for (let i = 0; i < amount_days; i++) {
-      const day_element = document.createElement('div');
-      day_element.classList.add('day');
-      day_element.textContent = i + 1;
+	for (let i = 0; i < amount_days; i++) {
+		const day_element = document.createElement("div");
+		day_element.classList.add("day");
+		day_element.textContent = i + 1;
 
-      if (selectedDay == (i + 1) && selectedYear == year && selectedMonth == month) {
-         day_element.classList.add('selected');
-      }
+		if (
+			selectedDay == i + 1 &&
+			selectedYear == year &&
+			selectedMonth == month
+		) {
+			day_element.classList.add("selected");
+		}
 
-      day_element.addEventListener('click', function () {
-         selectedDate = new Date(year + '-' + (month + 1) + '-' + (i + 1));
-         selectedDay = (i + 1);
-         selectedMonth = month;
-         selectedYear = year;
+		day_element.addEventListener("click", function () {
+			selectedDate = new Date(year + "-" + (month + 1) + "-" + (i + 1));
+			selectedDay = i + 1;
+			selectedMonth = month;
+			selectedYear = year;
 
-         selectedDateElement.textContent = formatDate(selectedDate);
-         selectedDateElement.dataset.value = selectedDate;
+			selectedDateElement.textContent = formatDate(selectedDate);
+			selectedDateElement.dataset.value = selectedDate;
 
-         populateDates();
-      });
+			populateDates();
+		});
 
-      daysElement.appendChild(day_element);
-   }
+		daysElement.appendChild(day_element);
+	}
 }
 
 // HELPER FUNCTIONS
-function checkEventPathForClass (path, selector) {
-   for (let i = 0; i < path.length; i++) {
-      if (path[i].classList && path[i].classList.contains(selector)) {
-         return true;
-      }
-   }
+function checkEventPathForClass(path, selector) {
+	for (let i = 0; i < path.length; i++) {
+		if (path[i].classList && path[i].classList.contains(selector)) {
+			return true;
+		}
+	}
 
-   return false;
+	return false;
 }
-function formatDate (d) {
-   let day = d.getDate();
-   if (day < 10) {
-      day = '0' + day;
-   }
+function formatDate(d) {
+	let day = d.getDate();
+	if (day < 10) {
+		day = "0" + day;
+	}
 
-   let month = d.getMonth() + 1;
-   if (month < 10) {
-      month = '0' + month;
-   }
+	let month = d.getMonth() + 1;
+	if (month < 10) {
+		month = "0" + month;
+	}
 
-   let year = d.getFullYear();
+	let year = d.getFullYear();
 
-   return day + ' / ' + month + ' / ' + year;
+	return day + " / " + month + " / " + year;
 }
